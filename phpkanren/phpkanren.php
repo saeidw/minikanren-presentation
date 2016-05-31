44340971229ee6e8cb620d85b03900909c2238ad
Saeid Al-Wazzan <saeid.wazzan@nonsingular.net>
2016-06-01 00:24:39 +0300

    Add some more definitions

--- phpkanren/phpkanren.php
+++ phpkanren/phpkanren.php
@@ -111,9 +111,130 @@ class Substitution {
     }
 }
 
+class State {
+    private $substitution;
+    private $counter;
+
+    public function __construct($substitution, $counter) {
+        $this->substitution = $substitution;
+        $this->counter = $counter;
+    }
+
+    public function getSubstitution() {
+        return $this->substitution;
+    }
+
+    public function getCounter() {
+        return $this->counter;
+    }
+}
+
+class Goal {
+    private $fn;
+
+    public function __construct(callable $fn) {
+        $this->fn = $fn;
+    }
+
+    public function execute(State $state) {
+        return $this->fn($state);
+    }
+}
+
+function unify($u, $v, Substitution $s) {
+    $u = $s->walk($u);
+    $v = $s->walk($v);
+
+    if (isVariable($u) && isVariable($v)) {
+        if ($u->equals($v)) {
+            return $s;
+        }
+    } else if (isVariable($u)) {
+        return $s->extend(new Mapping($u, $v));
+    } else if (isVariable($v)) {
+        return $s->extend(new Mapping($v, $u));
+    } else if (isPair($u) && isPair($v)) {
+        $s1 = unify($u->getFirst(), $v->getFirst(), $s);
+        $s2 = unify($u->getSecond(), $v->getSecond(), $s1);
+
+        return $s2;
+    } else if ($u === $v) {
+        return $s;
+    } else {
+        return null;
+    }
+}
+
+function mzero() {
+    return array();
+}
+
+function unit(State $state) {
+    return array($state);
+}
+
+function eq($u, $v) {
+    return new Goal(
+        function (State $state) use ($u, $v) {
+            $s = unify($u, $v, $state->getSubstitution());
+            if (isset($s)) {
+                return unit(new State($s, $state->getCounter()));
+            } else {
+                return mzero();
+            }
+        }
+    );
+}
+
+function callFresh(callable $fn) {
+    return new Goal(
+        function (State $state) use ($fn) {
+            $s = $state->getSubstitution();
+            $c = $state->getCounter();
+
+            $var = new Variable($c);
+            $newState = new State($s, ($c + 1));
+            $freshFn = $fn($c);
+
+            return $freshFn($newState);
+        }
+    );
+}
+
+function mplus($S1, $S2) {
+    if (!isset($S1)) {
+        return $S2;
+    }
+
+    if (is_callable($S1)) {
+        return function () use ($S1, $S2) {
+            return mplus($S2, $S1());
+        };
+    }
+
+    $first = \array_shift($S1);
+    return \array_merge($first, mplus($S2, $S1));
+}
+
+function bind($S, Goal $g) {
+    if (!isset($S)) {
+        return mzero();
+    }
+
+    if (is_callable($S)) {
+        return function () use ($S, $g) {
+            return bind($S(), $g);
+        };
+    }
+
+    $first = \array_shift($S);
+    return \array_merge($first, bind($S, $g));
+}
+
 $s = (new Substitution())
     ->extend(new Mapping(new Variable(0), new Variable(1)))
-    ->extend(new Mapping(new Variable(1), new Variable(2)));
+    ->extend(new Mapping(new Variable(1), new Variable(2)))
+    ->extend(new Mapping(new Variable(2), new Pair(new Variable(1), 5)));
 
 $r = $s->walk(new Variable(0));
 var_dump($r);
